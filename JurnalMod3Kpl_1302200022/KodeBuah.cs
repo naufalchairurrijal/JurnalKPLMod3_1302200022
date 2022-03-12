09a50296a25929d407e80e5fb017c2633fc8358a
naufalchairurrijal <naufalalifaza@student.telkomuniversity.ac.id>
2022-03-12 10:27:18 +0700

    Menambahkan table driven KodeBuah

---
+++ JurnalMod3Kpl_1302200022/KodeBuah.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JurnalMod3Kpl_1302200022
+{
+    public class KodeBuah
+    {
+        public enum NamaBuah 
+        {
+            Apel,
+            Aprikot,
+            Alpukat,
+            Pisang,
+            Paprika,
+            Blackberry,
+            Ceri,
+            Kelapa,
+            Jagung,
+            Kurma,
+            Durian,
+            Anggur,
+            Melon,
+            Semangka
+        };
+
+        public string Kode(NamaBuah buah)
+        {
+            string[] kbuah = { "A00", "B00", "C00", "D00", "E00", "F00", "H00", "I00", "J00", "K00", "L00", "M00", "N00", "O00" };
+
+            return kbuah[(int)buah];
+        }
+    }
+}
--- JurnalMod3Kpl_1302200022/Program.cs
+++ JurnalMod3Kpl_1302200022/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using JurnalMod3Kpl_1302200022;
+
+KodeBuah buah = new KodeBuah();
+
+Console.WriteLine(KodeBuah.NamaBuah.Apel);
+Console.WriteLine(buah.Kode(KodeBuah.NamaBuah.Apel));
+
+Console.WriteLine(KodeBuah.NamaBuah.Aprikot);
+Console.WriteLine(buah.Kode(KodeBuah.NamaBuah.Aprikot));
+
+Console.WriteLine(KodeBuah.NamaBuah.Alpukat);
+Console.WriteLine(buah.Kode(KodeBuah.NamaBuah.Alpukat));
+
+Console.WriteLine(KodeBuah.NamaBuah.Pisang);
+Console.WriteLine(buah.Kode(KodeBuah.NamaBuah.Pisang));
